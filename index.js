66a62424c8987a4f7230209e123e0f4410d8d695
Jeremy May <may.jeremy.r@gmail.com>
2020-09-24 10:00:28 -0700

    add 2nd property to boxes array

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const boxes = [
-    { name: 'red', color: 'red' },
-    { name: 'orange', color: 'orange' },
-    { name: 'yellow', color: 'yellow' },
-    { name: 'green', color: 'green' },
-    { name: 'blue', color: 'blue' },
-    { name: 'indigo', color: 'indigo' },
-    { name: 'violet', color: 'violet' },
+    { name: 'red', background: 'red', color: 'white' },
+    { name: 'orange', background: 'orange', color: 'black' },
+    { name: 'yellow', background: 'yellow', color: 'black' },
+    { name: 'green', background: 'green', color: 'white' },
+    { name: 'blue', background: 'blue', color: 'white' },
+    { name: 'indigo', background: 'indigo', color: 'white' },
+    { name: 'violet', background: 'violet', color: 'black' },
 ];
