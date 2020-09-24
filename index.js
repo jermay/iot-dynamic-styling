0a7ecec34a2a585793f0426900ab66c2bb6351a4
Jeremy May <may.jeremy.r@gmail.com>
2020-09-24 09:04:55 -0700

    initial template

---
+++ index.js
@@ -0,0 +1,9 @@
+const boxes = [
+    { name: 'red', color: 'red' },
+    { name: 'orange', color: 'orange' },
+    { name: 'yellow', color: 'yellow' },
+    { name: 'green', color: 'green' },
+    { name: 'blue', color: 'blue' },
+    { name: 'indigo', color: 'indigo' },
+    { name: 'violet', color: 'violet' },
+];
